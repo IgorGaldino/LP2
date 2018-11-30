cf2520c9cd71a93524db97ddefca81148525ce99
Igor Galdino <igormgaldino.com>
2018-11-30 17:44:09 -0200

    List

--- projeto2/Program.cs
+++ projeto2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace projeto
 {
@@ -8,6 +9,26 @@ namespace projeto
     {
         static void Main(string[] args)
         {
+            /* List Generic*/
+            /*
+                List<int> list = new List<int>();
+                list.Add(12);
+                list.Add(32);
+                list.Add(50);
+                list.Add("Teste");      //Erro! tipo inválido
+
+                int[] outralista = new int[] {1,2,3,4};
+                list.AddRange(outralista);
+
+                list.Insert(0,12);
+                list.Insert(1,32);
+                list.Insert(1,50);
+                list.Insert(3,44);
+                // list.Insert(10, 100);
+                Console.WriteLine("TEste: " + list[1]);
+                Console.WriteLine("TEste: " + list[2]);
+            */
+            /* Aluno com ArrayList */
             Alunos listaAlunos = new Alunos();
 
             Aluno alunoJose = new Aluno() {Codigo = 100, Nome ="Jose"};
